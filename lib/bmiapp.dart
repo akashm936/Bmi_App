bb8aafef06c3b4f6d25cae103b135dfecd6c1eeb
Akash Morwal <akashbhau101@gmail.com>
2025-04-28 11:16:38 +0530

    BMI App is Completed

--- lib/bmiapp.dart
+++ lib/bmiapp.dart
@@ -12,7 +12,7 @@ class _BmiAppState extends State<BmiApp> {
   var inchcontroller = TextEditingController();
 
   var result = "";
-  // var bgcolor ;
+  var bgcolor ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,19 +21,23 @@ class _BmiAppState extends State<BmiApp> {
         backgroundColor: ThemeData().appBarTheme.backgroundColor,
       ),
       body: Container(
-        // color: bgcolor!=null? bgcolor : Colors.purpleAccent.shade100 ,
+        color: bgcolor!=null? bgcolor : Colors.purpleAccent.shade200 ,
         child: Center(
           child: Container(
             width: 300,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Text("Calculate BMI",style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),),
+                SizedBox(height: 20,),
                 TextField(
                   controller: wtcontroller,
+                  keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: "Enter Your Weight",
                     // label: Text("Enter Your Weight ",style: TextStyle(fontSize: 16),),
                     prefixIcon: Icon(Icons.line_weight),
+
                     border: OutlineInputBorder(
                       borderSide: BorderSide(width: 3, color: Colors.black),
                     ),
@@ -42,6 +46,7 @@ class _BmiAppState extends State<BmiApp> {
                 SizedBox(height: 16),
                 TextField(
                   controller: fitcontroller,
+                  keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: "Enter Your height in fit",
                     // label: Text("Enter Your height in fit",style: TextStyle(fontSize: 16),),
@@ -54,6 +59,7 @@ class _BmiAppState extends State<BmiApp> {
                 SizedBox(height: 16),
                 TextField(
                   controller: inchcontroller,
+                  keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: "Enter Your Height in Inch",
                     // label: Text("Enter your Height in Inch",style: TextStyle(fontSize: 16),),
@@ -66,15 +72,15 @@ class _BmiAppState extends State<BmiApp> {
                 SizedBox(height: 16),
                 ElevatedButton(
                   onPressed: () {
-                    var Wt = wtcontroller.toString();
-                    var FitH = fitcontroller.toString();
-                    var Inch = inchcontroller.toString();
+                    var Wt = wtcontroller.text.toString();
+                    var FitH = fitcontroller.text.toString();
+                    var Inch = inchcontroller.text.toString();
 
                     if (Wt != "" && FitH != "" && Inch != "") {
 
-                      var iWt = int.parse(Wt);
-                      var iFit = int.parse(FitH);
-                      var iInch = int.parse(Inch);
+                      var iWt = double.parse(Wt);
+                      var iFit = double.parse(FitH);
+                      var iInch = double.parse(Inch);
 
                       var tInch = (iFit*12) + iInch;
                       var tCm = tInch * 2.54;
@@ -84,19 +90,22 @@ class _BmiAppState extends State<BmiApp> {
                       var msg = "";
                       if(bmi>25){
                         msg = "You're Overweight";
-                        // bgcolor = Colors.orange.shade100;
+                        bgcolor = Colors.orange.shade200;
                       }
                       else if(bmi<18){
                         msg = "Your are Underweight";
-                        // bgcolor = Colors.red.shade100;
+                        bgcolor = Colors.red.shade200;
                       }
                       else{
                         msg = "Your are Healthy";
-                        // bgcolor = Colors.green.shade100;
+                        bgcolor = Colors.green.shade200;
                     }
-
+                      print(result);
                       setState(() {
                         result = "Your Bmi is ${bmi.toStringAsFixed(2)}";
+                        wtcontroller.clear();
+                        fitcontroller.clear();
+                        inchcontroller.clear();
                       });
 
                     } else {
@@ -111,9 +120,7 @@ class _BmiAppState extends State<BmiApp> {
                   ),
                 ),
                 SizedBox(height: 20),
-                Text("$result",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                ),
+                Text(result, style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
               ],
             ),
           ),
